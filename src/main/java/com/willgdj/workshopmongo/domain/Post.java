967d759ae37d14bb8fdf918b89635c7b243d4541
Will gdj <Willgdj@gmail.com>
2023-10-23 20:01:34 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/willgdj/workshopmongo/domain/Post.java
+++ src/main/java/com/willgdj/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.willgdj.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.willgdj.workshopmongo.dto.AuthorDTO;
+import com.willgdj.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -73,6 +78,15 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -90,11 +104,5 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-	
-	
-	
-	
 
 }
